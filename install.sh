a6eab158e394c20300d01138e757a5bdf207cbf6
Beau <beau@beaumartinez.com>
2011-09-07 15:32:29 +0100

    Add installation script

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo 'Cloning Vundle...'
+git clone git://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+echo 'Installing plugins...'
+vim -u ~/.vim/vundle.vim +BundleInstall +q +q
+
+echo 'Symlinking .vimrc and .gvimrc...'
+ln -s ~/.vim/.vimrc ~/.vimrc
+ln -s ~/.vim/.gvimrc ~/.gvimrc
