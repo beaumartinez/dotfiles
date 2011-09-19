a02ed06652b6061a8a7b3ba42de92fbf7aa0f2d7
Beau <beau@beaumartinez.com>
2011-09-19 15:45:05 +0100

    Enhance install script

--- install.sh
+++ install.sh
@@ -1,3 +1,25 @@
+if [ -e ~/.vim -o -e ~/.vimrc -o -e ~/.gvimrc ]; then
+    DATE=$(date +%Y-%m-%d)
+    VIM_BACKUP_PATH=~/.vim-$DATE
+
+    echo Backing-up previous Vim config to $VIM_BACKUP_PATH...
+
+    if [ -e $VIM_BACKUP_PATH ]; then
+        echo Error: $VIM_BACKUP_PATH already exists
+
+        exit 1
+    fi
+
+    mkdir $VIM_BACKUP_PATH
+
+    mv ~/.vim $VIM_BACKUP_PATH
+    mv ~/.vimrc $VIM_BACKUP_PATH
+    mv ~/.gvimrc $VIM_BACKUP_PATH
+fi
+
+echo "Cloning Beau's Vim config..."
+git clone git://github.com/beaumartinez/.vim.git ~/.vim/
+
 echo 'Cloning Vundle...'
 git clone git://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
