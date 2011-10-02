274e2759be424422fb41a68b48756c2c3520730d
Beau <beau@beaumartinez.com>
2011-10-02 21:04:07 +0100

    Don't back-up existing Vim config

--- install.sh
+++ install.sh
@@ -1,28 +1,6 @@
 VIM_CONFIG_DIRECTORY=~/Documents/Code/vim-config
 
 echo Installing Beau\'s Vim config...
-
-if [ -e ~/.vim -o -e ~/.vimrc -o -e ~/.gvimrc ]; then
-    DATE=$(date +%Y-%m-%d)
-    VIM_BACKUP_PATH=~/.vim-$DATE
-
-    echo Existing Vim config found
-    echo Backing-up existing Vim config to $VIM_BACKUP_PATH...
-
-    if [ -e $VIM_BACKUP_PATH ]; then
-        echo Error: $VIM_BACKUP_PATH already exists
-        echo We couldn\'t back up your existing Vim config—do it yourself
-
-        exit 1
-    fi
-
-    mkdir $VIM_BACKUP_PATH
-
-    mv ~/.vim $VIM_BACKUP_PATH
-    mv ~/.vimrc $VIM_BACKUP_PATH
-    mv ~/.gvimrc $VIM_BACKUP_PATH
-fi
-
 echo Cloning Beau\'s Vim config...
 git clone git://github.com/beaumartinez/vim-config.git $VIM_CONFIG_DIRECTORY
 
