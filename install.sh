dd4e3e19a69991b6b219c067fdd8fca942198b45
Beau <beau@beaumartinez.com>
2013-01-08 14:46:15 +0000

    Jazz install script up

--- install.sh
+++ install.sh
@@ -1,25 +1,17 @@
-VIM_CONFIG_DIRECTORY=~/Documents/Code/vim-config
+#! /usr/bin/env bash
 
-echo Installing Beau\'s Vim config...
-echo Cloning Beau\'s Vim config...
-git clone git://github.com/beaumartinez/vim-config.git $VIM_CONFIG_DIRECTORY
+# Clone Vundle
+git clone git://github.com/gmarik/vundle.git bundle/vundle
 
-echo Cloning Vundle...
-git clone git://github.com/gmarik/vundle.git $VIM_CONFIG_DIRECTORY/bundle/vundle
+# Install plugins
+vim -u vundle.vim +BundleInstall! +qa
 
-echo Installing plugins...
-vim -u $VIM_CONFIG_DIRECTORY/vundle.vim +BundleInstall +qa
+# Symlink .vimrc and .gvimrc
+ln -s vim.vim ~/.vimrc
+ln -s gvim.vim ~/.gvimrc
 
-echo Symlinking Vim config...
-ln -s $VIM_CONFIG_DIRECTORY ~/.vim
-
-echo Symlinking .vimrc and .gvimrc...
-ln -s $VIM_CONFIG_DIRECTORY/vim.vim ~/.vimrc
-ln -s $VIM_CONFIG_DIRECTORY/gvim.vim ~/.gvimrc
-
-echo Making special directories...
+# Make special directories
+# TODO: Do this in vim.vim
 mkdir ~/.vim/.backup
 mkdir ~/.vim/.swap
 mkdir ~/.vim/.undo
-
-echo Done!
